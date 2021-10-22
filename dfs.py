685480d4c3399fe0d7ed95fab3f38a96958fdac7
dazedcodes <techwithmaya@gmail.com>
2021-10-21 22:46:16 -0500

    starter code for program

---
+++ dfs.py
@@ -0,0 +1,8 @@
+
+def main():
+    import sys
+    print ""
+    print "We're going to implement depth-first search using an adjaceny list as the data structure!"
+    print ""
+if __name__ == '__main__':
+    main()
